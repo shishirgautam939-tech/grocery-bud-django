8322736dce5a6beda2dd3f959a434d25624ed6ea
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-02-25 10:33:20 +0545

    add grocery item features

--- grocery/urls.py
+++ grocery/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('toggle/<int:item_id>/', views.toggle_completed, name='toggle'),
     path('update/<int:item_id>/', views.update_item, name='update'),
     path('delete/<int:item_id>/', views.delete_item, name='delete'),
+     path('add/', views.add_item, name='add'),
 ]
